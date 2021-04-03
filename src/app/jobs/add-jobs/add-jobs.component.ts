15667b096183d677f1a006ac461bc96f822cd5de
AmineMissaoui <amine.missaoui@esprit.tn>
2021-04-03 11:45:00 +0100

    update form

--- src/app/jobs/add-jobs/add-jobs.component.ts
+++ src/app/jobs/add-jobs/add-jobs.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Jobs } from 'src/app/model/jobs.model';
+import { Sector } from 'src/app/model/sector.model';
+import { EntrepriseService } from 'src/app/shared/entreprise.service';
 import { JobsService } from 'src/app/shared/jobs.service';
 
 @Component({
@@ -13,13 +15,25 @@ export class AddJobsComponent implements OnInit {
 
   job : Jobs;
   nbrApplications : number ;
-  viewersNbr : number 
-  constructor(private _jobsService: JobsService, private _router: Router) { }
+  viewersNbr : number;
+  listSectors : Sector;
+  sectorId : number;
+
+  constructor(private _jobsService: JobsService, private _router: Router, private _entrepriseService : EntrepriseService) { }
 
   ngOnInit(): void {
     this.job = new Jobs();
     this.job.nbrApplications = 0;
     this.job.viewersNbr = 0;
+    this._entrepriseService.getSectors().subscribe(data => {
+      this.listSectors = data;
+    })
+  }
+
+  selectOption(value){
+    console.log(value);
+      this.sectorId = value;
+      this.job.secteurId = this.sectorId;
   }
 
 
